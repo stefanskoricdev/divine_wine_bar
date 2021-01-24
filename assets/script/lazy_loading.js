52680f850fb45eedb063892c94ac142fb4156a1b
Stefan Skoric <stephanskoric@gmail.com>
2021-01-24 18:53:10 +0100

    Used intersection observer API to trigger animations and lazy loading for better performance. Added partials inside my style folder for easier maintainance of project

---
+++ assets/script/lazy_loading.js
@@ -0,0 +1,46 @@
+const header = document.querySelector("header");
+const mainWrapper = document.querySelector(".main-wrapper");
+const homePage = document.getElementById("home-page-wrapper");
+const aboutUsPage = document.getElementById("about-us-page-wrapper");
+const AboutUsPageContainers = aboutUsPage.querySelectorAll(".container");
+
+const hpOptions = {
+  threshold: 0,
+  rootMargin: "0px",
+};
+const options = {
+  threshold: 0,
+  rootMargin: "0% 0% -60% 0%",
+};
+
+const homePageObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      homePage.classList.add("active");
+    }
+  });
+}, hpOptions);
+
+const aboutUsPageContainerObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, options);
+
+homePageObserver.observe(homePage);
+AboutUsPageContainers.forEach((container) => {
+  aboutUsPageContainerObserver.observe(container);
+});
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset;
+  if (scrollTop === 0) {
+    header.classList.remove("active");
+    header.style.animation = "header-animation 500ms linear";
+  } else if (scrollTop > 400) {
+    header.classList.add("active");
+    header.style.animation = "sticky-header-animation 500ms linear";
+  }
+}); //Event changes header element appearance based on scroll position!
