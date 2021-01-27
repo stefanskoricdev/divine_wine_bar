5f6e487a2069e4d8d6b8e727ad3e0d8b677ba537
Stefan Skoric <stephanskoric@gmail.com>
2021-01-27 18:53:18 +0100

    Finished tablet responsive layout. Started mobile responsive layout.

--- assets/script/lazy_loading.js
+++ assets/script/lazy_loading.js
@@ -4,6 +4,8 @@ const homePage = document.getElementById("home-page-wrapper");
 const aboutUsPage = document.getElementById("about-us-page-wrapper");
 const AboutUsPageContainers = aboutUsPage.querySelectorAll(".container");
 
+const mql = window.matchMedia("(min-width: 768px)");
+
 const hpOptions = {
   threshold: 0,
   rootMargin: "0px",
@@ -34,13 +36,21 @@ AboutUsPageContainers.forEach((container) => {
   aboutUsPageContainerObserver.observe(container);
 });
 
-window.addEventListener("scroll", () => {
-  let scrollTop = window.pageYOffset;
-  if (scrollTop === 0) {
-    header.classList.remove("active");
-    header.style.animation = "header-animation 500ms linear";
-  } else if (scrollTop > 400) {
-    header.classList.add("active");
-    header.style.animation = "sticky-header-animation 500ms linear";
+const screenSizeMatch = () => {
+  if (mql.matches) {
+    window.addEventListener("scroll", () => {
+      let scrollTop = window.pageYOffset;
+      if (scrollTop === 0) {
+        header.classList.remove("active");
+        header.style.animation = "header-animation 500ms linear";
+      } else if (scrollTop > 400) {
+        header.classList.add("active");
+        header.style.animation = "sticky-header-animation 500ms linear";
+      }
+    }); //Event changes header element appearance based on scroll position!
+  } else {
+    return;
   }
-}); //Event changes header element appearance based on scroll position!
+};
+screenSizeMatch();
+window.addEventListener("resize", screenSizeMatch);
