8f2b5161da938cdf1425474d603f0a94c8692177
Stefan Skoric <stephanskoric@gmail.com>
2021-01-30 09:50:15 +0100

    Updated layout. Added background to web pages.

--- assets/script/lazy_loading.js
+++ assets/script/lazy_loading.js
@@ -42,7 +42,7 @@ const screenSizeMatch = () => {
       let scrollTop = window.pageYOffset;
       if (scrollTop === 0) {
         header.classList.remove("active");
-        header.style.animation = "header-animation 500ms linear";
+        header.style.animation = "header-animation 200ms linear";
       } else if (scrollTop > 400) {
         header.classList.add("active");
         header.style.animation = "sticky-header-animation 500ms linear";
